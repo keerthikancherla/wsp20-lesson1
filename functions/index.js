368d9bf42eb3ddc58e7f3852788bbe2346b22406
Rishi <rishi.kumar.kancherla@footlocker.com>
2020-01-14 11:19:24 +0530

    backendHandler added

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,21 @@ const express = require('express')
 const app = express()
 
 exports.httpReq = functions.https.onRequest(app)
-app.get('/', requestHandler);
-app.get('/home', requestHandler);
-app.get('/login', requestHandler);
-function requestHandler(request, response){
+app.get('/', frontendHandler);
+app.get('/b', backendHandler);
+app.get('/home', frontendHandler);
+app.get('/login', frontendHandler);
+function frontendHandler(request, response) {
     response.sendFile(__dirname + '/spa/index.html')
+}
+function backendHandler(req, res){
+    const n = Math.random()
+    const num = Math.floor(n * 10)
+    let s = 0;
+    s = 50 + 12;
+    let page = `
+        <h1>Your lucky number is ${num}</h1>
+        Sum is ${s}
+    `;
+    res.send(page)
 }
